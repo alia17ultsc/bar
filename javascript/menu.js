5e8a9ffd8eda9c6d80102eac480dff8a489396c3
alia17ultsc <aliafayez2009@gmail.com>
2024-06-01 17:50:15 +0300

    update barmenu

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,7 +2,13 @@ showdrink(1);
 
 
 function showdrink(drinktype){
-    console.log("fgfgdgdf");
+   var menuitems=document.getElementsByClassName("menuitem");
+    for (var i=0; i<menuitems.length; i++){
+        menuitems[i].style.backgroundColor="white";
+        menuitems[i].style.color="black";
+        menuitems[i].style.fontWeight="normal";
+    }
+    
     var alldrinks=document.getElementsByClassName("drink");
     for (var i=0; i<alldrinks.length; i++){
         alldrinks[i].style.display="none";
@@ -11,13 +17,21 @@ function showdrink(drinktype){
     if (drinktype==1){
         type="coffee";
     }
+    else if(drinktype==2){
+        type = "tea";
+    }
+    else if(drinktype==3){
+        type = "alchoholic"
+    }
     else if(drinktype==4){
         type = "non-alchoholic";
     }
     else if(drinktype==5){
         type = "soft-drink";
     }
-    
+    else if(drinktype==6){
+        type = "juices";
+    }
     showspecificdrink(type);  
 }
 
@@ -26,4 +40,8 @@ function showspecificdrink(type){
         for (var i=0; i<drinks.length; i++){
         drinks[i].style.display="flex";
         }
+    var menuitem=document.getElementById(type);
+    menuitem.style.backgroundColor="gold";
+    menuitem.style.color="darkred";
+    menuitem.style.fontWeight="bold";
 }
