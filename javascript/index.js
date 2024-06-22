df0fc366f8dd2959c0a039d1a9af265af630809b
alia17ultsc <aliafayez2009@gmail.com>
2024-06-22 17:30:12 +0300

    adding water and bottles

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,16 @@
-window.addEventListener('scroll', e=>{document.body.style.cssText=`--scrollTop:${this.scrollY}px`})
+window.addEventListener('scroll', e=>{document.body.style.cssText=`--scrollTop:${gety()}px`})
+ 
+
+
+function gety(){
+    if(this.scrollY>220){
+        return 220;
+    }
+    else{
+        return this.scrollY;
+    }
+}
+
+function clickup(){
+    document.documentElement.scrollTop=0;
+}
